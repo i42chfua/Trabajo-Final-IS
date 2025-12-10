5c8176ae7d7f8177eb43baba22437b63953a6810
unknown <i42camoa@uco.es>
2025-12-10 11:16:44 +0100

    Pruebas Unitarias Adrian + Config Proyecto

--- PruebasUnitarias/test.cpp
+++ PruebasUnitarias/test.cpp
@@ -42,3 +42,50 @@ TEST_F(TestTutorias, LoginIncorrecto) {
     // Debe devolver ID -1 (Usuario vacío/error)
     EXPECT_EQ(resultado.id, -1);
 }
+
+// Prueba 3: Asignación Automática
+TEST_F(TestTutorias, AsignacionAutomatica) {
+    Database db("test_tutorias.db");
+
+    // Crear usuarios
+    db.registrarUsuario(Usuario(0, "Profe", "T1", "p", "TUTOR", 0));
+    db.registrarUsuario(Usuario(0, "Alumno", "A1", "p", "ALUMNO", 0));
+
+    // IDs reales
+    int idT = db.login("T1", "p").id;
+    int idA = db.login("A1", "p").id;
+
+    // Ejecutar lógica
+    EXPECT_TRUE(db.asignarTutorAutomaticamente(idA));
+
+    // Verificar
+    Usuario alumno = db.login("A1", "p");
+    EXPECT_EQ(alumno.id_vinculado, idT);
+}
+
+// Prueba 4: Reasignación Manual (Compleja)
+TEST_F(TestTutorias, ReasignacionManual) {
+    Database db("test_tutorias.db");
+
+    // 1 Alumno, 2 Tutores
+    db.registrarUsuario(Usuario(0, "A", "A", "p", "ALUMNO", 0));
+    db.registrarUsuario(Usuario(0, "T1", "T1", "p", "TUTOR", 0));
+    db.registrarUsuario(Usuario(0, "T2", "T2", "p", "TUTOR", 0));
+
+    int idA = db.login("A", "p").id;
+    int idT1 = db.login("T1", "p").id;
+    int idT2 = db.login("T2", "p").id;
+
+    // Asignar a T1
+    db.asignarTutorManual(idA, idT1);
+
+    // Cambiar a T2
+    EXPECT_TRUE(db.asignarTutorManual(idA, idT2));
+
+    // Verificar: Alumno tiene a T2, T1 está libre (0)
+    Usuario alumno = db.login("A", "p");
+    Usuario tutor1 = db.login("T1", "p");
+
+    EXPECT_EQ(alumno.id_vinculado, idT2);
+    EXPECT_EQ(tutor1.id_vinculado, 0);
+}
